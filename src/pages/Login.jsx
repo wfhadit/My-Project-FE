b2fedfee8d89665ea4984715a0e81acdc9e62227
wfhadit <wfhadit@gmail.com>
2025-03-07 15:30:55 +0700

    feat(auth):integration register and login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import { login } from "../api/auth";
 import Logo from "../assets/Untitled-9.svg";
+import { useState } from "react";
 const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await login(form);
+      const token = response.data.token;
+      if (token) {
+        localStorage.setItem("token", token);
+
+        setLoading(true);
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="container col-xl-10 col-xxl-8 px-4 py-5">
       <div className="row align-items-center g-lg-5 py-5">
@@ -7,13 +44,19 @@ const Login = () => {
           <img src={Logo} alt="" />
         </div>
         <div className="col-md-10 mx-auto col-lg-5">
-          <form className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+          <form
+            className="p-4 p-md-5 border rounded-3 bg-body-tertiary"
+            onSubmit={handleSubmit}
+          >
             <div className="form-floating mb-3">
               <input
                 type="email"
                 className="form-control"
                 id="floatingInput"
                 placeholder="name@example.com"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
               />
               <label htmlFor="floatingInput">Email</label>
             </div>
@@ -23,11 +66,28 @@ const Login = () => {
                 className="form-control"
                 id="floatingPassword"
                 placeholder="Password"
+                name="password"
+                value={form.password}
+                onChange={handleChange}
               />
               <label htmlFor="floatingPassword">Password</label>
             </div>
-            <button className="w-100 btn btn-lg btn-dark" type="submit">
-              Login
+            <button
+              className="w-100 btn btn-lg btn-dark"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    aria-hidden="true"
+                  ></span>
+                  <span role="status">Loading...</span>
+                </>
+              ) : (
+                "Login"
+              )}
             </button>
             <hr className="my-4" />
             <small className="text-body-secondary">
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import { register } from "../api/auth";
 import Logo from "../assets/Untitled-9.svg";
+import { useState } from "react";
 
 const Register = () => {
+  const [form, setForm] = useState({
+    nama: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await register(form);
+      console.log(data);
+      setLoading(true);
+      setTimeout(() => {
+        navigate("/login");
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="container col-xl-10 col-xxl-8 px-4 py-2">
       <div className="row align-items-center g-lg-5 py-5">
@@ -8,13 +40,19 @@ const Register = () => {
           <img src={Logo} alt="" />
         </div>
         <div className="col-md-10 mx-auto col-lg-5">
-          <form className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+          <form
+            className="p-4 p-md-5 border rounded-3 bg-body-tertiary"
+            onSubmit={handleSubmit}
+          >
             <div className="form-floating mb-3">
               <input
                 type="text"
                 className="form-control"
                 id="floatingText"
-                placeholder="Elisabeth Sparkle"
+                placeholder="nama"
+                name="nama"
+                value={form.nama}
+                onChange={handleChange}
               />
               <label htmlFor="floatingText">Nama</label>
             </div>
@@ -24,6 +62,9 @@ const Register = () => {
                 className="form-control"
                 id="floatingInput"
                 placeholder="name@example.com"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
               />
               <label htmlFor="floatingInput">Email</label>
             </div>
@@ -33,11 +74,28 @@ const Register = () => {
                 className="form-control"
                 id="floatingPassword"
                 placeholder="Password"
+                name="password"
+                value={form.password}
+                onChange={handleChange}
               />
               <label htmlFor="floatingPassword">Password</label>
             </div>
-            <button className="w-100 btn btn-lg btn-dark" type="submit">
-              Register
+            <button
+              className="w-100 btn btn-lg btn-dark"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    aria-hidden="true"
+                  ></span>
+                  <span role="status">Loading...</span>
+                </>
+              ) : (
+                "Register"
+              )}
             </button>
             <hr className="my-4" />
             <small className="text-body-secondary">

5db96dff20d51e6ef5705fe497fb184cda7913d0
wfhadit <wfhadit@gmail.com>
2025-04-01 10:49:26 +0700

    feat(int):integration with be

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:1300",
+  // baseURL: "http://localhost:1300",
+  baseURL: "https://my-project-be-production.up.railway.app/",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("auth")}`,
   },
